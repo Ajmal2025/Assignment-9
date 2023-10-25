9647c8f593b7975a324f7cb5bf0042237700c309
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-25 21:48:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//9.	Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number.
+// Print that message.
+let favorite_Number = 7;
+console.log(`My Favorite Number is ${favorite_Number}`);
---
+++ index.ts
@@ -0,0 +1,8 @@
+
+
+//9.	Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number.
+// Print that message.
+
+let favorite_Number:number= 7;
+console.log(`My Favorite Number is ${favorite_Number}`);
